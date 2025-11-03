494f4691c30fd3442dc92667eefdb4de56fc2cc9
samnewdich <samnewdich@gmail.com>
2025-11-03 21:33:31 +0100

    added a method in Api route

--- src/main/java/com/samlinux/myapp/controller/ApiController.java
+++ src/main/java/com/samlinux/myapp/controller/ApiController.java
@@ -1,10 +1,19 @@
 package com.samlinux.myapp.controller;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api")
 public class ApiController{
-    public String read(){
-        return "God is greatest";
+    
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestBody Map<String, String> data) {
+        //TODO: process POST request
+        return Map.of("response", "success");
     }
+    
 }
